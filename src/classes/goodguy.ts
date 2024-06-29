4144f9ced244e0de0addd9dd37a30eb89a03e90a
Scott McDonald <scottocorp@gmail.com>
2024-06-29 20:05:00 +1000

    GoodGuy refactor: cleanup & making props private, with getters & setters

--- src/classes/goodguy.ts
+++ src/classes/goodguy.ts
@@ -11,65 +11,73 @@ export enum GoodGuyStatus {
 // The GoodGuy class is used to represent the user, or "good guy" in the game.
 export class GoodGuy {
   
-  public context: any;
-  public xPos: number;
-  public vertices: any;
-  public status: GoodGuyStatus;
-  public alpha: number;
+  private _context: any;
+  private _xPos: number;
+  private _vertices: any;
+  private _status: GoodGuyStatus;
+  private _alpha: number;
 
   constructor(
-    public canvas: any,
-    public goodGuyXRangeStart: number,
-    public goodGuyXRangeWidth: number,
-    public yPos: number,
-    public goodGuySpeed: number,
-    public colour: string,
+    canvas: any,
+    private _goodGuyXRangeStart: number,
+    private _goodGuyXRangeWidth: number,
+    private _yPos: number,
+    private _goodGuySpeed: number,
+    private _color: string,
 	) {
 
-    this.context = canvas.ctx;
+    this._context = canvas.ctx;
 
     // Place the goodGuy squarely in the middle of the range that has been assigned.
-    this.xPos = this.goodGuyXRangeStart + 0.5 * this.goodGuyXRangeWidth-10;
+    this._xPos = this._goodGuyXRangeStart + 0.5 * this._goodGuyXRangeWidth-10;
 
-    this.vertices = [{ x: 0, y: 0 }, { x: 10, y: 17 }, { x: -10, y: 17}];
+    this._vertices = [{ x: 0, y: 0 }, { x: 10, y: 17 }, { x: -10, y: 17}];
     //this.vertices = [{ x: 0, y: 0 }, { x: 20, y: 34 }, { x: -20, y: 34}];
 
-    this.status = GoodGuyStatus.STATIONARY;
-    this.alpha = 1.0;
+    this._status = GoodGuyStatus.STATIONARY;
+    this._alpha = 1.0;
 
     this.create();
   }
 
-  public create = function () {
+  private create = function () {
 
     // First we take a backup of the context...
-    this.context.save();
+    this._context.save();
   
     // Because we are going to translate it to the object's x and y position...  
-    this.context.translate(this.xPos, this.yPos);
+    this._context.translate(this._xPos, this._yPos);
   
     // And draw a polygon, the vertices of which are stored in this.vertices
-    this.context.fillStyle = "rgba(" + this.colour + ", " + this.alpha + ")";  
-    this.context.beginPath();
+    this._context.fillStyle = "rgba(" + this._color + ", " + this._alpha + ")";  
+    this._context.beginPath();
   
-    this.context.moveTo(this.vertices[0].x, this.vertices[0].y);
+    this._context.moveTo(this._vertices[0].x, this._vertices[0].y);
   
-    var length = this.vertices.length;
+    let length = this._vertices.length;
   
-    for (var i = 1; i <= length; i++) {
+    for (let i = 1; i <= length; i++) {
   
       if (i < length) {
-        this.context.lineTo(this.vertices[i].x, this.vertices[i].y);
+        this._context.lineTo(this._vertices[i].x, this._vertices[i].y);
       } else {
-        this.context.lineTo(this.vertices[0].x, this.vertices[0].y);
+        this._context.lineTo(this._vertices[0].x, this._vertices[0].y);
       }
     }
   
-    this.context.closePath();
-    this.context.fill();
+    this._context.closePath();
+    this._context.fill();
   
     // And once we're done, we just restore the context...
-    this.context.restore();
+    this._context.restore();
   
-  };
+  }
+
+  public get status() {
+    return this._status;
+  }
+
+  public set status(status: GoodGuyStatus) {
+    this._status = status;
+  }
 }
